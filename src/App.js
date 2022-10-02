8eeb91cb1c298669cb4128805c098547c7275ea6
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-10-02 13:40:42 +0800

    Implemented accessing child component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
 import React, { Component } from 'react';
-import Editor from './components/Editor';
-import ProductTable from './components/ProductTable';
-import { ColorInvalidElements } from './components/jQueryColorizer';
+import FormField from './components/FormField';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      products: []
-    };
-    this.editorRef = React.createRef();
+    this.fieldRef = React.createRef();
   }
 
-  addProduct = (product) => {
-    if(this.state.products.indexOf(product.name) === -1) {
-      this.setState({ products: [...this.state.products, product] });
-    }
-  }
-
-  colorFields = () => {
-    ColorInvalidElements(this.editorRef.current);
+  handleClick = () => {
+    this.fieldRef.current.focus();
   }
 
   render() {
     return (
-      <div>
+      <div className="m-2">
+        <FormField label="Name" fieldRef={ this.fieldRef } />
         <div className="text-center m-2">
-          <button className="btn btn-primary" onClick={ this.colorFields }>
-            jQuery
+          <button className="btn btn-primary" onClick={ this.handleClick }>
+            Focus
           </button>
-        </div>
-        <div ref={ this.editorRef }>
-          <Editor callback={ this.addProduct } />
-        </div>
-        <h6 className="bg-secondary text-white m-2 p-2">Products</h6>
-        <div className="m-2">
-          {
-            this.state.products.length === 0 ? <div className="text-center">No Products</div>
-            : <ProductTable products={ this.state.products } />
-          }
         </div>
       </div>
     )
